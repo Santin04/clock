39482a182b74cedaa78ceecb356a19da206910b3
Santin04 <masantinte@gmail.com>
2023-08-17 17:38:06 -0300

    finihs clock

---
+++ relogioscript.js
@@ -0,0 +1,37 @@
+const escrever_hora = document.getElementById('horario');
+const escrever_dia = document.getElementById('dia')
+
+function definir_hora() {
+    let present = new Date;
+    let hours = present.getHours();
+    let minutes = present.getMinutes();
+    let seconds = present.getSeconds();
+    let day = present.getDate();
+    let month = present.getMonth();
+    let year = present.getFullYear();
+
+    if (seconds < 10){
+        seconds = "0" + seconds
+    }
+
+    if (minutes < 10){
+        minutes = "0" + minutes
+    }
+
+    if (hours < 10){
+        hours = "0" + hours
+    }
+
+    if (day < 10){
+        day = "0" + day
+    }
+
+    if (month < 10){
+        month = "0" + month
+    }
+
+    escrever_hora.innerHTML = `${hours}:${minutes}:${seconds}`;
+    escrever_dia.innerHTML = `${day}/${month}/${year}`;
+}
+
+setInterval(definir_hora, 1000)
